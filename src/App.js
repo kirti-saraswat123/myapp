c88ba43f09d62883801d71fdb8af33f3762c85ae
kirti saraswat <kkrtisaraswat789@gmail.com>
2024-02-26 18:31:03 +0530

    new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Nav, NavItem, NavLink } from "reactstrap";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+      <Nav>
+        <NavItem >
+          <NavLink active href="#" >
+            SHOP
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink disabled href="#">
+            NEW IN
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink disabled href="#">
+            BRANDS
+          </NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink disabled href="#">
+            GIFT
+          </NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink disabled href="#">
+            OFFER
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Ccard from './Ccard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Ccard/>
   </React.StrictMode>
 );
 
